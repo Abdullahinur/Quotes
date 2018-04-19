8cc13c7982306d2d6b79fc36967d1853c38dc10a
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-19 09:29:36 +0300

    generate quote component display mock quote add some css

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quote = 'This is my first Quote';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
