1ccd6a2aa92c420dd9750192c9a10a2637bd7233
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-19 10:52:44 +0300

    generate quote-detail to display details above quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { FormsModule } from '@angular/forms';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote-model';
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+@Input() quote: Quote;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
