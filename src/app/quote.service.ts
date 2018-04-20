a3831dcf59df7d1fcc5921e86db07907c93b93bb
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-20 22:36:24 +0300

    add expand get quotes finction to work with api and pipe to handle errors

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service'
-
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class QuoteService {
@@ -12,8 +12,9 @@ export class QuoteService {
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
     getQuotes(): Observable<Quote[]> {
-
-          return this.http.get<Quote[]>(this.quotesUrl);
+      return this.http.get<Quote[]>(this.quotesUrl)
+        .pipe(tap(quotes => this.log(`fetched quotes`))
+          catchError(this.handlerError('getQuotes', [])));
 
 
 }
@@ -29,4 +30,24 @@ export class QuoteService {
     this.messageService.add('QuoteService: ' + message);
   }
 
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handlerError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+    }
+  }
 }
